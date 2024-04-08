bed6980f63965bcd1ebead748a6360c050364d62
ruben <rubenjosearaujo@icloud.com>
2024-04-08 12:29:46 +0100

    made a new server.js to test the get request of the JSON thorugh the fetch API, it worked

---
+++ server02.js
@@ -0,0 +1,47 @@
+import express from "express";
+import fetch from "file-fetch";
+
+const app = express();
+
+const port = 3000;
+
+async function fetchData() {
+  try {
+    const response = await fetch("equipamentos.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const file = await response.json();
+    return file;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Rethrow the error to handle it further
+  }
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const file = await fetchData();
+    res.render("index.ejs", { data: file });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/equipment/:category", async (req, res) => {
+  try {
+    const file = await fetchData();
+    const category = req.params.category;
+    if (file.hasOwnProperty(category)) {
+      res.json(file[category]);
+    } else {
+      res.status(404).send("Category not found");
+    }
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
